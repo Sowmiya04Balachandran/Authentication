15174c4cac938e246220f1fb94823fa358341443
Sowmiya04Balachandran <sowmiya2641@gmail.com>
2023-09-15 12:32:13 +0530

    tokenId manage

---
+++ src/store/AuthContext.js
@@ -0,0 +1,35 @@
+import  React,{useState} from 'react';
+
+const AuthContext=React.createContext({
+    token:'',
+    isLoggedIn:false,
+    login:(token)=>{},
+    logout:()=>{},
+})
+
+export const AuthContextProvider=(props)=>{
+   const[token,setToken]=useState(null);
+
+   const userIsLoggedIn=!!token;
+
+
+const loggedInHandler=(token)=>{
+setToken(token)
+}
+
+const loggedOutHandler=()=>{
+setToken(null);
+}
+
+const contextValue={
+token:token,
+isLoggedIn:userIsLoggedIn,
+login:loggedInHandler,
+logout:loggedOutHandler,
+}
+
+    return(
+        <AuthContext.Provider value={contextValue}>{props.children}</AuthContext.Provider>
+    )
+}
+export default AuthContext;
